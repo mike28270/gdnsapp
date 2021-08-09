9db094de4f2a167a3e0f9a8b0fce84d171b93fe0
mike28270 <p44362007@gmail.com>
2021-08-09 16:26:31 +0700

    Fix bug: Input wrong URL or DNS doesn't exist .

--- gdnsapp/index.js
+++ gdnsapp/index.js
@@ -6,43 +6,54 @@ var host = 'dns.google';
 var url = '';
 var response = '';
 
-app.get('/:id', (req, res) => {
-	const { id } = req.params;
+try {
+	app.get('/:id', (req, res) => {
+		const { id } = req.params;
 
-	url = '/resolve?name=' + id + '&type=A';
-  console.log('Interpreting DNS{ '+host+url+' } into IP Address.\n');
+		url = '/resolve?name=' + id + '&type=A';
+	  console.log('Interpreting DNS{ '+host+url+' } into IP Address.\n');
 
-  //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-  var options = {
-      host: host,
-      port: '443',
-      path: url
-  };
+	  //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+	  var options = {
+	      host: host,
+	      port: '443',
+	      path: url
+	  };
 
-  function callback(response) {
-    var str = '';
+	  function callback(response) {
+	    var str = '';
+			var json = '';
+			var data = '';
 
-    //another chunk of data has been received, so append it to `str`
-    response.on('data', function (chunk) {
-      str += chunk;
-    });
+	    //another chunk of data has been received, so append it to `str`
+	    response.on('data', function (chunk) {
+	      str += chunk;
+	    });
 
-    //the whole response has been received, so we just print it out here
-    response.on('end', function () {
-      var json = JSON.parse(str);
-      var answer = JSON.stringify(json.Answer[0]);
-      var json = JSON.parse(answer);
-      var data = json.data;
-      console.log('Got response ---->');
-      console.log(str+'\n');
-      console.log('Got result ---->');
-      console.log(data+'\n');
-      console.log('---------------------------');
-      res.status(200).send(data);
-    });
-  }
-  response = https.request(options, callback).end();
-});
+	    //the whole response has been received, so we just print it out here
+	    response.on('end', function () {
+	      json = JSON.parse(str);
+				console.log('Got response ---->');
+				console.log(str+'\n');
+				if ('Answer' in json) {
+	      	answer = JSON.stringify(json.Answer[0]);
+	      	json = JSON.parse(answer);
+	      	data = json.data;
+				} else {
+					data = 'The DNS doesn\'t exist.';
+				}
+	      console.log('Got result ---->');
+	      console.log(data+'\n');
+	      console.log('---------------------------');
+	      res.status(200).send(data);
+	    });
+	  }
+	  https.request(options, callback).end();
+	});
+} catch (e) {
+	console.log(e);
+	res.status(500).send(e);
+}
 
 app.listen(port, "0.0.0.0", () => {
     console.log(`Now listening on port ${port}`);
